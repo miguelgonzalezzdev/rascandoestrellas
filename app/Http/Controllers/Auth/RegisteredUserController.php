885565e11b8939829c4584a0dd6359a008f8b9a9
mgongar2703 <mgongar2703@g.educaand.es>
2023-06-19 23:31:31 +0200

    Procfile mailpiterror

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -49,7 +49,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        //event(new Registered($user));
 
         Auth::login($user);
 

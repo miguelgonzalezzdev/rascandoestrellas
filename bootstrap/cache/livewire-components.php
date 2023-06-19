763c5ef821e381f89e354db5fd40d0fdee4dd410
mgongar2703 <mgongar2703@g.educaand.es>
2023-06-20 00:38:00 +0200

    bootstrap

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'elemento-home' => 'App\\Http\\Livewire\\ElementoHome',
+  'elemento-novedad-home' => 'App\\Http\\Livewire\\ElementoNovedadHome',
+  'elemento-seccion' => 'App\\Http\\Livewire\\ElementoSeccion',
+  'like-post' => 'App\\Http\\Livewire\\LikePost',
+  'resena' => 'App\\Http\\Livewire\\Resena',
+  'resena-elemento-principal' => 'App\\Http\\Livewire\\ResenaElementoPrincipal',
+);
